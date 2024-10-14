3764d34bae5a60d9802d636b4894ca6d4d0707f2
Ivan <fedirko.ivan@gmail.com>
2024-10-15 00:10:42 +0700

    Adding Led example

--- examples/led_example.c
+++ examples/led_example.c
@@ -3,24 +3,19 @@
 #include "nrf_delay.h"
 #include "nrf_gpio.h"
 
+#define led1 13
+#define led2 14
 
-#define  led1 13
-#define  led2 14
-
-int led_example(void)
-{
-  nrf_gpio_cfg_output(led1); 
+int led_example(void) {
+  nrf_gpio_cfg_output(led1);
   nrf_gpio_cfg_output(led2);
 
-  while(1)
-  {
+  while (1) {
     nrf_gpio_pin_set(led1); // Set logic 1 to pin 13
     nrf_delay_ms(100);
     nrf_gpio_pin_clear(led1); // Set pin to logic 0
     nrf_delay_ms(100);
 
     nrf_gpio_pin_toggle(led2);
-   }
-
-
+  }
 }
