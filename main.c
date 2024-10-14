f621fd8fc6ecaf6c8086954962b8041167db541b
Ivan <fedirko.ivan@gmail.com>
2024-10-15 00:12:00 +0700

    Adding common project fixes

--- main.c
+++ main.c
@@ -2,10 +2,12 @@
 #include <stdint.h>
 #include "led_board_example.h"
 #include "led_example.h"
+#include "di_example.h"
+#include "rtt_example.h"
 
 
 int main(void)
 {
-    led_example();
+    rtt_example();
 }
 
