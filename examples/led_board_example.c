8b1428f368153de8792b6e66883b42e572cc6d14
Ivan <fedirko.ivan@gmail.com>
2024-10-16 08:19:56 +0700

    Delay toggle to led example

--- examples/led_board_example.c
+++ examples/led_board_example.c
@@ -12,6 +12,10 @@ int led_board_example(void)
     /* Toggle LEDs. */
     while (true)
     {
+        bsp_board_led_on(0);
+        nrf_delay_ms(400);
+        bsp_board_led_off(0);
+        nrf_delay_ms(400);
         for (int i = 0; i < LEDS_NUMBER; i++)
         {
             bsp_board_led_invert(i);
